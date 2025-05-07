70d9e64c9b514eb2741d27ea29d6a7f2496d989a
JJ <orallojohnjames@gmail.com>
2025-05-07 16:53:54 +0900

    from mac changes, bare NPC tweaks

--- npc/scripts/patrol_location.gd
+++ npc/scripts/patrol_location.gd
@@ -1,6 +1,10 @@
-@tool
+# patrol_location.gd
 
+@tool
 class_name PatrolLocation extends Node2D
+
+signal transform_changed
+
 
 @export var wait_time: float = 0.0:
 	set(v):
@@ -10,8 +14,17 @@ class_name PatrolLocation extends Node2D
 var target_position: Vector2 = Vector2.ZERO
 
 
+func _enter_tree() -> void:
+	set_notify_transform(true)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSFORM_CHANGED:
+		transform_changed.emit()
+
 func _ready() -> void:
 	target_position = global_position
+	_update_wait_time_label()
+	
 	if Engine.is_editor_hint():
 		return
 	$Sprite2D.queue_free()
@@ -22,6 +35,12 @@ func update_label(_s:String) -> void:
 	
 	pass
 
+
+func update_line(next_location: Vector2) -> void:
+	var line: Line2D = $Sprite2D/Line2D
+	line.points[1] = next_location - position
+	pass
+	
 
 
 func _update_wait_time_label() -> void:
