22b51d528a10e0650e3fd3740a50f13d40f39053
notjj1234 <orallojohnjames@gmail.com>
2025-06-07 21:01:25 +0900

    Fixed that stupid thing when debug fks over everything

--- gui/dialog_system/scripts/dialog_system.gd
+++ gui/dialog_system/scripts/dialog_system.gd
@@ -1,4 +1,10 @@
 @tool
 @icon("res://gui/dialog_system/icons/star_bubble.svg")
-
 class_name DialogSystemNode extends CanvasLayer
+
+func _ready():
+    if Engine.is_editor_hint():
+        visible = false  # or queue_free() if you prefer
+        return
+
+    # your actual setup logic goes here
